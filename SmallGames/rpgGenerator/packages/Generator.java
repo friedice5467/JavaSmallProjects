6d71cbe68e867c55a34c528cf0309db1889d200d
Danny Nguyen <58054670+friedice5467@users.noreply.github.com>
2022-03-25 17:12:36 -0700

    Delete SmallGames/rpgGenerator/packages directory

--- SmallGames/rpgGenerator/packages/Generator.java
+++
@@ -1,7 +0,0 @@
-package SmallGames.rpgGenerator.packages;
-
-public class Generator {
-    private String name;
-    private Role role;
-
-}
--- SmallGames/rpgGenerator/packages/UserInput.java
+++
@@ -1,9 +0,0 @@
-package SmallGames.rpgGenerator.packages;
-
-import java.util.Scanner;
-
-public class UserInput {
-    Scanner scanner = new Scanner(System.in);
-
-
-}
